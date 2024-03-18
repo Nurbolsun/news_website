aeb6d999862126f27c77d9fdf67bc99147630792
Nurbolsun <kg.nurbo.97@mail.ru>
2024-03-18 17:00:47 +0600

    sort, category, views

--- news/urls.py
+++ news/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from news.views import *
 
 urlpatterns = [
-    path('slider/', MainAPIView.as_view(), name='slider'),
+    path('slider/', SliderAPIViewSet.as_view({'get': 'list'}), name='slider'),
+    path('slider/<int:pk>', SliderAPIViewSet.as_view({'get': 'retrieve'})),
+
     path('newses/', AllNewsAPIView.as_view(), name='all-news'),
     path('news-category/', NewsCategoryViewSet.as_view({'get': 'list'}), name='all-categories'),
     path('news-category/<int:pk>', NewsCategoryViewSet.as_view({'get': 'retrieve'})),
     path('news-category/create/', NewsCategoryViewSet.as_view({'post': 'create'})),
+    path('news-by-category/<int:pk>', NewsByCategoryAPIView.as_view(), name='news-by-category'),
+    path('news-by-data/', SortNewsByDataAPI.as_view(), name='news-by-data'),
+    path('news-by-view/', MostViewerNews.as_view(), name='news-by-view'),
     path('tag/<int:pk>', TagViewSet.as_view({'get': 'retrieve'})),
     path('tag/', TagViewSet.as_view({'get': 'list'})),
     path('tag/create/', TagViewSet.as_view({'post': 'create'})),
--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status, generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from news.serializers import NewsListSerializers, NewsCategoryListSerializer, TagListSerializer, SliderSerializer
 from .models import News, NewsCategory, Tag, Slider
@@ -9,7 +10,6 @@ from .models import News, NewsCategory, Tag, Slider
 class NewsAPIList(generics.ListCreateAPIView):
     queryset = News.objects.all()
     serializer_class = NewsListSerializers
-    # permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class NewsAPIUpdate(generics.RetrieveUpdateAPIView):
@@ -28,6 +28,28 @@ class NewsCategoryViewSet(viewsets.ModelViewSet):
     serializer_class = NewsCategoryListSerializer
 
 
+class NewsByCategoryAPIView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        category = get_object_or_404(NewsCategory, pk=pk)
+        news = News.objects.filter(news_category=category)
+        news_serializer = NewsListSerializers(news, many=True)
+        return Response(news_serializer.data)
+
+
+class SortNewsByDataAPI(APIView):
+    def get(self, request, *args, **kwargs):
+        last_news = News.objects.all().order_by('-created_at')[:5]
+        last_news_serializer = NewsListSerializers(last_news, many=True)
+        return Response(last_news_serializer.data)
+
+
+class MostViewerNews(APIView):
+    def get(self, request, *args, **kwargs):
+        most_viewed_news = News.objects.all().order_by('-count_views')[:5]
+        most_viewed_serializer = NewsListSerializers(most_viewed_news, many=True)
+        return Response(most_viewed_serializer.data)
+
+
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagListSerializer
@@ -40,20 +62,9 @@ class AllNewsAPIView(APIView):
         return Response(serializer.data)
 
 
-class MainAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        slider = Slider.objects.all()
-        news = News.objects.all()
-
-        slider_serializer = SliderSerializer(slider, many=True)
-        news_serializer = NewsListSerializers(news, many=True)
-
-        data_slider = slider_serializer.data
-
-        for slider_item in data_slider:
-            slider_item['news'] = news_serializer.data
-
-        return Response(data_slider)
+class SliderAPIViewSet(viewsets.ModelViewSet):
+    queryset = Slider.objects.all()
+    serializer_class = SliderSerializer
 
 
 class NewsDetailView(APIView):
